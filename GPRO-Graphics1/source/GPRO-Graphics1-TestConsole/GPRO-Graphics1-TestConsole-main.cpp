08850299878af450afa2b53a4071bff65d2b44d2
reece.cristea <reece.cristea@mymail.champlain.edu>
2020-09-07 11:49:31 -0700

    Very start of project

--- GPRO-Graphics1/source/GPRO-Graphics1-TestConsole/GPRO-Graphics1-TestConsole-main.cpp
+++ GPRO-Graphics1/source/GPRO-Graphics1-TestConsole/GPRO-Graphics1-TestConsole-main.cpp
@@ -25,9 +25,11 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <iostream>
 
 #include "gpro/gpro-math/gproVector.h"
+
+using namespace std;
 
 
 void testVector()
@@ -56,6 +58,29 @@ void testVector()
 int main(int const argc, char const* const argv[])
 {
 	testVector();
+
+	// Image
+
+	const int image_width = 256;
+	const int image_height = 256;
+
+	// Render
+
+	cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+	for (int j = image_height - 1; j >= 0; --j) {
+		for (int i = 0; i < image_width; ++i) {
+			auto r = double(i) / (image_width - 1);
+			auto g = double(j) / (image_height - 1);
+			auto b = 0.25;
+
+			int ir = static_cast<int>(255.999 * r);
+			int ig = static_cast<int>(255.999 * g);
+			int ib = static_cast<int>(255.999 * b);
+
+			cout << ir << ' ' << ig << ' ' << ib << '\n';
+		}
+	}
 
 	printf("\n\n");
 	system("pause");
